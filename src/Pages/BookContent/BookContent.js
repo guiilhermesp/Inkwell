8a28ea2800c55753f09195e22e3e0eb5b0421356
Guilherme Pereira <guiilherme.pereira27@gmail.com>
2023-10-10 23:42:53 -0300

    Feat: responsiveness on bookContent page and Card component

--- src/Pages/BookContent/BookContent.js
+++ src/Pages/BookContent/BookContent.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Head from "../../Components/Helper/Head";
 import styles from "./BookContent.module.css";
 
 const BookContent = () => {
   const { state } = useLocation();
-  const source = `https://covers.openlibrary.org/b/olid/${state.cover_edition_key}-L.jpg`;
-console.log('state:', state)
+  const [isResponsive, setIsResponsive] = useState()
+  const source = `https://covers.openlibrary.org/b/olid/${state.cover_edition_key}-${isResponsive ?'L':'M' }.jpg`;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsResponsive(window.innerWidth >= 751);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className={styles.page}>
       <Head title="Book" description={state.title + " page"} />
@@ -15,6 +28,7 @@ console.log('state:', state)
         <img 
         src={source}
         alt={`${state.title} cover`}
+        className={styles.cover}
         />
       </div>
       <div className={styles.details}>
